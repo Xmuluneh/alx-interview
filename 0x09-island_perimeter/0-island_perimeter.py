d49a8e0a36261328f6b2fa1dd92a0f27c6c9b939
Xmuluneh <mulabr673@gmail.com>
2023-02-28 22:48:37 +0300

    Tak Complete

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,32 +7,16 @@ def island_perimeter(grid):
        Args:
             -grid: the first parameter
     """
-    per_meter = 0
+    p = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j]:
-                per_meter = per_meter + numberOfWater(grid, i, j)
-
-    return per_meter
-
-
-def numberOfWater(grid, i, j):
-    """Returns the number of water neighbors a cell has in a grid.
-       Args:
-            -grid:matrix
-            -i :row
-            -j:columns
-    """
-
-    number = 0
-
-    if i <= 0 or not grid[i - 1][j]:
-        number += 1
-    if j <= 0 or not grid[i][j - 1]:
-        number += 1
-    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
-        number += 1
-    if i >= len(grid) - 1 or not grid[i + 1][j]:
-        number += 1
-
-    return
+            if (grid[i][j] == 1):
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    p += 1
+                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                    p += 1
+                if (j <= 0 or grid[i][j - 1] == 0):
+                    p += 1
+                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
+                    p += 1
+    return p
